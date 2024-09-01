424b36087fd865a9afb4695646addab17138cf7a
hilarylawson <hilarylawson@unomaha.edu>
2024-09-01 20:43:17 +0000

    Finished FirstProgram lab

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,22 +1,25 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Hilary Lawson
+#Date: 8/28/24
+#Assignment: Lab 1
 
 def main():
-  print("First Program")
+  #print("First Program")
   #Say hello
-  
+  print("hello!")
   #Ask for the user's name
-
+  name = input("What is your name?")
   #Use the user's name in the program.
-
+  print("Nice to meet you", name)
   #Ask the user for their age.
-
+  age = input("How old are you?")
   #Tell the user what year they were born in.
+  age = int(age)
   #Assume that they have not had their birthday yet this year.
+  birthYear = 2024 - age
+  print("You were born in", birthYear)
 
-
+  
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
--- MadLib.py
+++ MadLib.py
@@ -6,7 +6,10 @@
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun1 = input("Enter a noun")
+  noun2 = input("Enter a noun")
+  adj1 = input("Enter a noun")
+  
   #Print the story with the user supplied words.
 
 
